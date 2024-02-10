8a49dffef73f56357eb259fe5f455c1c05a386db
ktpanda2025 <108114336+ktpanda2025@users.noreply.github.com>
2024-02-09 23:54:27 -0800

    added new data files

---
+++ Og_data/spotify_api.py
@@ -0,0 +1,35 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import keys
+import pandas as pd
+
+df = pd.read_csv('Og_data/Spotify_chart_song_ranks.csv')
+unique_track_uris = df['uri'].unique()
+sub_list =[]
+
+for i in range(0,len(unique_track_uris),5):
+    sub_list.append(unique_track_uris[i:i+5])
+
+
+client_id =keys.client_id 
+client_secret=keys.client_secret
+
+# Authenticate with Spotify API
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+audio_fets = []
+hhh = 0
+
+
+for j in sub_list:
+    audio_features = sp.audio_features(j)
+    audio_fets.append(audio_features)
+    print("hhhhfdhfd",hhh)
+    hhh+=1
+
+
+audio_features_df = pd.DataFrame(audio_fets)
+
+audio_features_df.to_csv('Spotify_audio_features.csv', index=False)
+print(audio_features_df.head())
