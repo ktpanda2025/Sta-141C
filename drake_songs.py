a4e7af7273000ae32075928518237c953e807b32
ktpanda2025 <108114336+ktpanda2025@users.noreply.github.com>
2024-02-13 16:28:34 -0800

    drake subset data

---
+++ drake_songs.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+
+df = pd.read_csv('Drake_Spotify_Data.csv')
+# i want to see the column names
+print(df.columns)
+
+subset = df[['track_uri', 'energy', 'album_release_year']][df['album_release_year'] >= 2020].sort_values(by='album_release_year')
+#make it order by release year ascending
+#save it to a new csv file
+subset.to_csv('Drake_Spotify_Data_2020.csv', index=False)
