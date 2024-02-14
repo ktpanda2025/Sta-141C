969fe2515a9c7edba1de6c4996fb7df5bbd84200
ktpanda2025 <108114336+ktpanda2025@users.noreply.github.com>
2024-02-13 19:59:11 -0800

    idk

--- drake_songs.py
+++ drake_songs.py
@@ -7,4 +7,4 @@ print(df.columns)
 subset = df[['track_uri', 'energy', 'album_release_year']][df['album_release_year'] >= 2020].sort_values(by='album_release_year')
 #make it order by release year ascending
 #save it to a new csv file
-subset.to_csv('Drake_Spotify_Data_2020.csv', index=False)
+print(subset)
